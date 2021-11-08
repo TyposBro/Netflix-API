9becccdc1f457688fc81deff05af1d164ab56732
@khasanjonovich <khasanjonovich@gmail.com>
2021-11-08 20:36:09 +0900

    adding cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import router from "./routes/Router.js";
 const app = express();
@@ -19,6 +20,7 @@ mongoose
   });
 
 app.use(express.json());
+app.use(cors());
 app.use("/api", router);
 app.listen(3001, () => {
   console.log("Server's Running");
