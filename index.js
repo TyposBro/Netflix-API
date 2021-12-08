d116a58f94aedca5e3c1997c4c1c1fd6fe3db12a
Azizbek Umidjonov <azizbek@unist.ac.kr>
2021-12-08 17:02:03 +0900

    add compression

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import compression from "compression";
 
 import router from "./routes/Router.js";
 const app = express();
@@ -21,6 +22,7 @@ mongoose
 
 app.use(express.json());
 app.use(cors());
+app.use(compression());
 app.use("/api", router);
 app.listen(process.env.PORT || 3001, () => {
   console.log("Server's Running");
