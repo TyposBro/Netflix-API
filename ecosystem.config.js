c6cb8bf6ca28c0e34b7bdec4cb10c045b9dc9fa6
Azizbek Umidjonov <azizbek@unist.ac.kr>
2021-12-08 17:33:12 +0900

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: "netflix-api",
+      script: "node index.js",
+      watch: ".",
+      instances: "max",
+    },
+    {
+      script: "./service-worker/",
+      watch: ["./service-worker"],
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "SSH_USERNAME",
+      host: "SSH_HOSTMACHINE",
+      ref: "origin/master",
+      repo: "GIT_REPOSITORY",
+      path: "DESTINATION_PATH",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
